2ba2665a02971de207946b8d825555768db0c7f1
haysonc <haysonc@stu.otc.school.nz>
2021-03-19 13:08:57 +1300

    Coded program to display points for each team and total point. Task One

---
+++ main.py
@@ -0,0 +1,16 @@
+match1 = 3
+match2 = 6
+match3 = 10
+match4 = 10
+match5 = 6
+
+total_points = match1 + match2 + match3 + match4 + match5
+
+print("""Points scored in match one:{}
+Points scored in match two:{}
+Points scored in match three:{}
+Points scored in match four:{}
+Points scored in match five:{}
+""".format(match1, match2, match3, match4, match5))
+
+print("The total amount of points scored was: {}".format(total_points))
